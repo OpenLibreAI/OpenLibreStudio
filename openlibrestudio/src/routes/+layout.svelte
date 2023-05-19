ed21d8f7f8660fa116efbcbb7917e509038a0471
arunabhdas <arunabhdas@gmail.com>
2023-05-19 19:38:32 -0400

    Added gradient background

--- openlibrestudio/src/routes/+layout.svelte
+++ openlibrestudio/src/routes/+layout.svelte
@@ -1,3 +1,11 @@
 <div class="mainContainer">
 	<slot />
 </div>
+
+<style>
+    .mainContainer {
+        min-height: 100vh;
+        background: radial-gradient( circle 1215px at 23.4% -21.8%,  rgba(9,29,85,1) 0.2%, rgba(0,0,0,1) 200.2% );
+    }
+</style>
+
