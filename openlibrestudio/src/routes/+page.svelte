b056d12451f11d3a9efb4b2a34f878322c63659d
arunabhdas <arunabhdas@gmail.com>
2023-05-20 04:09:41 -0400

    Created Login form

--- openlibrestudio/src/routes/+page.svelte
+++ openlibrestudio/src/routes/+page.svelte
@@ -1,27 +1,22 @@
-
 <script>
-	import Authenticate from "../components/Authenticate.svelte";
- </script>
- 
- <Authenticate/>
+	import Authenticate from '../components/Authenticate.svelte';
+</script>
+
+<Authenticate />
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2"></h2>
+		<h2 class="h2 pt-4" >Please Login</h2>
 
 		<div class="flex justify-center space-x-2">
-			<a
-				class="btn variant-filled"
-				href="#"
-				target="_blank"
-				rel="noreferrer"
-			>
-				Get Started
-			</a>
+			<input class="input variant-form-material" type="text" placeholder="Email" />
 		</div>
-		<div class="space-y-2">
+		<div class="flex justify-center space-x-2">
+			<input class="input variant-form-material" type="text" placeholder="Password" />
+		</div>
+		<a class="btn variant-filled" href="#" target="_blank" rel="noreferrer"> Login </a>
 
-		</div>
+		<div class="space-y-2" />
 	</div>
 </div>
 
@@ -35,8 +30,7 @@
 	}
 	.img-bg {
 		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
-			glow 5s linear infinite;
+		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite, glow 5s linear infinite;
 	}
 	@keyframes glow {
 		0% {
