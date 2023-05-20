dda4290baedff210607b564cd48d8274cc6a04c5
arunabhdas <arunabhdas@gmail.com>
2023-05-20 02:49:58 -0400

    Ran ==> npx svelte-add@latest tailwindcss

--- openlibrestudio/src/routes/+layout.svelte
+++ openlibrestudio/src/routes/+layout.svelte
@@ -1,11 +1,18 @@
+<script>
+	import '../app.postcss';
+</script>
+
 <div class="mainContainer">
 	<slot />
 </div>
 
 <style>
-    .mainContainer {
-        min-height: 100vh;
-        background: radial-gradient( circle 1215px at 23.4% -21.8%,  rgba(9,29,85,1) 0.2%, rgba(0,0,0,1) 200.2% );
-    }
+	.mainContainer {
+		min-height: 100vh;
+		background: radial-gradient(
+			circle 1215px at 23.4% -21.8%,
+			rgba(9, 29, 85, 1) 0.2%,
+			rgba(0, 0, 0, 1) 200.2%
+		);
+	}
 </style>
-
